1410cbd4bff49d388d91b722a217d44b905d86ce
Jonathan R <jreyes.satx@gmail.com>
2015-10-02 15:26:30 -0500

    switch exercise

---
+++ public/js/switch_js.js
@@ -0,0 +1,36 @@
+// ignore these lines for now
+// just know that the variable 'color' will end up with a random value from the colors array
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+var response = ['blood', 'fruit', 'lemons', 'limes', 'the sky', '\'', '\'', 'my humor', 'an artist', 'coffee', 'sand'];
+var indexResponse = colors.indexOf(color);
+var workAround = "The color " + color + ' reminds me of ' + response[indexResponse] + '.';
+
+
+switch (color) {
+	case 'red':
+		console.log(workAround)
+		break; 
+	case 'orange':
+		console.log(workAround)
+		break; 
+	case 'yellow':
+		console.log(workAround)
+		break; 
+	case 'green':
+		console.log(workAround)
+		break; 
+	case 'blue':
+		console.log(workAround)
+		break; 
+	default:
+		console.log('I do not know anything by that color.');
+	}
+
+
+    // todo: create a case statement that will handle every color except indigo and violet
+    // todo: when a color is encountered log a message that tells the color, and an object of that color
+    //       example: Blue is the color of the sky.
+
+    // todo: create a default case that will catch indigo and violet
+    // todo: for the default case, log: I do not know anything by that color.
