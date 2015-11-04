dce4fec0c54f6a575484540422157582a9823b07
Jonathan R <jreyes.satx@gmail.com>
2015-11-04 15:21:06 -0600

    fizz buzz with args or prompts for user entered values

--- public/php/fizz_buzz.php
+++ public/php/fizz_buzz.php
@@ -1,13 +1,31 @@
 <?php
-    fwrite(STDOUT, 'Please enter a starting numeric value: ');
-    $startValue = trim(fgets(STDIN));
-    fwrite(STDOUT, 'Please enter a ending numeric value: ');
-    $endValue = trim(fgets(STDIN));
-    fwrite(STDOUT, 'Please enter a numeric increment value: ');
-    $increment = trim(fgets(STDIN));
+    if ($argc < 2) {
+        fwrite(STDOUT, 'Please enter a starting numeric value: ');
+        $startValue = trim(fgets(STDIN));
+        if (!is_numeric($startValue)) {
+            die('NOT a numeric value');
+        }
+    } else {
+        $startValue = $argv[1];
+    }
 
-    if (empty($increment)) {
-        $increment = 1;
+    if ($argc < 3) {
+        fwrite(STDOUT, 'Please enter a ending numeric value: ');
+        $endValue = trim(fgets(STDIN));
+        if (!is_numeric($endValue)) {
+            die('NOT a numeric value');
+        }
+    } else {
+        $endValue = $argv[2];
+    }
+    if ($argc < 4) {
+        fwrite(STDOUT, 'Please enter a numeric increment value: ');
+        $increment = trim(fgets(STDIN));
+        if (empty($increment)) {
+            $increment = 1;
+        }
+    } else {
+        $increment = $argv[3];
     }
 
 
