78dad634f5b1c7f918b0829d5035c6c4dab46d8e
Jonathan R <jreyes.satx@gmail.com>
2015-11-04 14:17:33 -0600

    added user prompt for start num and end num, exercise pt1

--- public/php/fizz_buzz.php
+++ public/php/fizz_buzz.php
@@ -1,10 +1,18 @@
 <?php
-        for ($i = 1; $i <= 100; $i++) {
-        if ($i % 3 === 0 && $i % 5 === 0) {
+    fwrite(STDOUT, 'Please enter a starting numeric value ');
+    $startValue = trim(fgets(STDIN));
+    var_dump($startValue);
+    fwrite(STDOUT, 'Please enter a ending numeric value ');
+    $endValue = trim(fgets(STDIN));
+    var_dump($endValue);
+
+
+    for ($i = $startValue ; $i <= $endValue ; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
             echo "FizzBuzz\n";
-        } else if ($i % 3 === 0) {
+        } else if ($i % 3 == 0) {
             echo "Fizz\n";
-        } else if ($i % 5 === 0) {
+        } else if ($i % 5 == 0) {
             echo "Buzz\n";
         } 
         else {
