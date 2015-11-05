09daac4301237069f3dafe522420b5458fad0173
Jonathan R <jreyes.satx@gmail.com>
2015-11-05 15:51:41 -0600

    modulus function add-on, function exercise part next

--- public/php/arithmetic.php
+++ public/php/arithmetic.php
@@ -20,9 +20,15 @@ function divide($a, $b)
     return $a / $b;
 }
 
+function modulus($a, $b)
+{
+    return $a % $b;
+}
+
 $a = mt_rand ( 1 , 100 );
 $b = mt_rand ($a, 100 );
 
+echo 'The random numbers to math are ' . $a . ' & ' . $b . PHP_EOL;
 echo add($a, $b) . PHP_EOL;
 echo subtract($a, $b) . PHP_EOL;
 echo multiply($a, $b) . PHP_EOL;
