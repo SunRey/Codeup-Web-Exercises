c28d905b0306394486f137f8a5b7c8e52db9287c
Jonathan R <jreyes.satx@gmail.com>
2015-11-05 00:28:07 -0600

    Last part of the foreach exercise...to bed ye!

--- public/php/foreach.php
+++ public/php/foreach.php
@@ -3,26 +3,24 @@
     $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
     foreach ($things as $junk) {
     
-        // if (is_array($junk)) {
-        //     echo "This is an array\n";
-        //     // foreach ($junk as $what_is) {
-        //     //     echo $what_is . "\n";
-        //     // }
-        // } elseif (is_integer($junk)) {
-        //     echo "{$junk} is a integer\n";
-        // } elseif (is_bool($junk)) {
-        //     echo "{$junk} is a boolean\n";
-        // } elseif (is_float($junk)) {
-        //     echo "{$junk} could be a float... ";
-        //     if (is_double($junk)) {
-        //         echo "AND {$junk} could also be a double\n";
-        //     }
-        // } elseif (is_null($junk)) {
-        //     echo "{$junk} has a null value\n";
-        // } elseif (is_string($junk)) {
-        //     echo "I am the catch all string machine\n";
-        // }
-        if (is_scalar($junk)) {
-            echo "{$junk} is a type of scalar?\n";
+        if (is_array($junk)) {
+            echo "Array (  ";
+            foreach ($junk as $what_is) {
+                echo "{$what_is}\t";
+            }
+            echo " )\n";
+        } elseif (is_integer($junk)) {
+            echo "{$junk}\n";
+        } elseif (is_bool($junk)) {
+            echo "{$junk}\n";
+        } elseif (is_float($junk)) {
+            echo "{$junk}\n";
+        } elseif (is_null($junk)) {
+            echo "\n";
+        } elseif (is_string($junk)) {
+            echo "{$junk}\n";
         }
+        // if (is_scalar($junk)) {
+        //     echo "{$junk} is a type of scalar?\n";
+        // }
     }
