8df02c665c02808d044721fa8c3a552cc598e815
Jonathan R <jreyes.satx@gmail.com>
2015-11-05 18:55:53 -0600

    modified exercise with better random num and terneries for subtract, division, and modulo function

--- public/php/arithmetic.php
+++ public/php/arithmetic.php
@@ -7,7 +7,7 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
-    return $a - $b;
+    return ($a >= $b) ? ($a - $b) : ($b - $a);
 }
 
 function multiply($a, $b)
@@ -17,19 +17,21 @@ function multiply($a, $b)
 
 function divide($a, $b)
 {
-    return $a / $b;
+    return ($a >= $b) ? ($a / $b) : ($b / $a);
 }
 
 function modulus($a, $b)
 {
-    return $a % $b;
+    return ($a >= $b) ? ($a % $b) : ($b % $a);
 }
 
 $a = mt_rand ( 1 , 100 );
-$b = mt_rand ($a, 100 );
+$b = mt_rand ($a, ($a + 100) );
 
+echo PHP_EOL;
 echo 'The random numbers to math are ' . $a . ' & ' . $b . PHP_EOL;
-echo add($a, $b) . PHP_EOL;
-echo subtract($a, $b) . PHP_EOL;
-echo multiply($a, $b) . PHP_EOL;
-echo divide($a, $b) . PHP_EOL;
+echo add($a, $b) . ' : is the sum.' . PHP_EOL;
+echo subtract($a, $b) . ' : is the difference.' . PHP_EOL;
+echo multiply($a, $b) . ' : is the product.' . PHP_EOL;
+echo divide($a, $b) . ' : is the quotion.' . PHP_EOL;
+echo modulus($a, $b) . " : is the remainder.\n" . PHP_EOL;
