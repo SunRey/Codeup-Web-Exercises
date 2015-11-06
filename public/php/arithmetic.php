8eef6bfe7507f33fa63faf145aac83bfc424b95c
Jonathan R <jreyes.satx@gmail.com>
2015-11-06 11:35:27 -0600

    fisinshed aritmatic exercise

--- public/php/arithmetic.php
+++ public/php/arithmetic.php
@@ -1,32 +1,34 @@
 <?php
 
-$a = mt_rand ( 1 , 100 );
-$b = mt_rand ($a, ($a + 100) );
-
-if ($argc == 2) {
+if ($argc == 1) {
+    $a = mt_rand ( 1 , 100 );
+    $b = mt_rand ($a, ($a + 100) );
+} elseif ($argc == 2) {
     if (validNumber($argv[1])) {
         $a = (int)$argv[1];
+        $b = mt_rand ($a, ($a + 100) );
     } else {
         giveThemError();
     }
-}
-if ($argc >= 3) {
+} elseif ($argc == 3) {
     if (validNumber($argv[1]) && validNumber($argv[2])) {
         $a = (int)$argv[1];
         $b = (int)$argv[2];
     } else {
         giveThemError();
     }
+} else {
+    die('TOO MANY args passed it; like a Tur-duc-ken!' . PHP_EOL;);
 }
 
 
 function giveThemError () {
-    echo "Make sure your number(s) are not 0 and only numeric values\n";
+    echo "Make sure your number(s) are only numeric values!!\n";
     exit(0);
 }
 
 function validNumber ($num) {
-    if ($num != 0 && is_numeric($num)) {
+    if (is_numeric($num)) {
         return true;
     } else {
         return false;
@@ -35,34 +37,42 @@ function validNumber ($num) {
 
 function add($a, $b)
 {
-    return $a + $b;
+    return ($a + $b) . ' : is the sum.' . PHP_EOL;
 }
 
 function subtract($a, $b)
 {
-    return ($a >= $b) ? ($a - $b) : ($b - $a);
+    return ($a >= $b) ? ($a - $b) . ' : is the difference.' . PHP_EOL : ($b - $a) . ' : is the difference.' . PHP_EOL;
 }
 
 function multiply($a, $b)
 {
-    return $a * $b;
+    return ($a * $b)  . ' : is the product.' . PHP_EOL;
 }
 
 function divide($a, $b)
 {
-    return ($a >= $b) ? ($a / $b) : ($b / $a);
+    if ($a == 0 || $b == 0) {
+        return "We don't DO 0" . PHP_EOL;;
+    } else {
+        return ($a >= $b) ? ($a / $b)  . ' : is the quotion.' . PHP_EOL : ($b / $a)  . ' : is the quotion.' . PHP_EOL;
+    }
 }
 
 function modulus($a, $b)
 {
-    return ($a >= $b) ? ($a % $b) : ($b % $a);
+    if ($a == 0 || $b == 0) {
+        return "We don't DO 0" . PHP_EOL;
+    } else {
+        return ($a >= $b) ? ($a % $b)  . " : is the remainder.\n" . PHP_EOL : ($b % $a)  . " : is the remainder.\n" . PHP_EOL;
+    }
 }
 
 echo PHP_EOL;
 echo 'The random numbers to math are ' . $a . ' & ' . $b . PHP_EOL;
-echo add($a, $b) . ' : is the sum.' . PHP_EOL;
-echo subtract($a, $b) . ' : is the difference.' . PHP_EOL;
-echo multiply($a, $b) . ' : is the product.' . PHP_EOL;
-echo divide($a, $b) . ' : is the quotion.' . PHP_EOL;
-echo modulus($a, $b) . " : is the remainder.\n" . PHP_EOL;
+echo add($a, $b);
+echo subtract($a, $b);
+echo multiply($a, $b);
+echo divide($a, $b);
+echo modulus($a, $b);
 
