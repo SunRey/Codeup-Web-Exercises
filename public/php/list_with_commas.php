f63f2b986edb9f7c5603baaec8f3043d6eeb37ed
Jonathan R <jreyes.satx@gmail.com>
2015-11-09 15:57:58 -0600

    next step for explod/implode exercise

--- public/php/list_with_commas.php
+++ public/php/list_with_commas.php
@@ -1,6 +1,7 @@
 <?php
     $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
     $physicists_array = explode(', ', $physicists_string);
+    sort($physicists_array);
     $physicists_array = addAndString($physicists_array);
     $famousFakePhysicists = implode(', ', $physicists_array);
     echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
