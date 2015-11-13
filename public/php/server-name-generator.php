66439a31a531035d70ccdf6cb3852b7424a70211
Jonathan R <jreyes.satx@gmail.com>
2015-11-13 13:39:12 -0600

    semi-working server name generator.

--- public/php/server-name-generator.php
+++ public/php/server-name-generator.php
@@ -1,26 +1,35 @@
 <?php 
-$adjs = [
-    'asinine', 'exquisite', 'serious', 'enlightened', 'clever', 
-    'sarcastic', 'quizzical', 'secular', 'beautiful', 'mysterious'
-];
-$nouns = [
-    'ass', 'mosquito', 'polecat', 'corpse', 'truth',
-    'bitch', 'teacher', 'blood', 'highway', 'virus'
-];
 
 function random_name ($adjs, $nouns) 
 {
     return $adjs[array_rand($adjs)] . '-' . $nouns[array_rand($nouns)];
 }
 
-function all_random_names ($arr1, $arr2) 
+function randomNamesArray ($arr1, $arr2) 
 {
     $randomNamesArray = array();
     for($i = 0; $i < 11; $i++)
     {
-        $randomNamesArray = random_name($arr1, $arr2);
-    }    
+        $randomNamesArray["name$i"] = random_name($arr1, $arr2);
+    }  
+ 
     return $randomNamesArray;
+}
+
+function pageController ()
+{
+    $adjs = [
+        'asinine', 'exquisite', 'serious', 'enlightened', 'clever', 
+        'sarcastic', 'quizzical', 'secular', 'beautiful', 'mysterious'
+    ];
+    $nouns = [
+        'ass', 'mosquito', 'polecat', 'corpse', 'truth',
+        'bitch', 'teacher', 'blood', 'highway', 'virus'
+    ];
+
+    $randomNames = randomNamesArray($adjs, $nouns);
+
+    return $randomNames;
 }
 
 ?>
@@ -36,13 +45,12 @@ function all_random_names ($arr1, $arr2)
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     </head>
     <body>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-md-offset-2"></div>
+                <div class="col-md-1 col-md-offset-2"></div>
                 <div class="col-md-6">
                     <h1>Your random server name</h1>
                 </div>
-                <div class="col-md-offset-4"></div>
             </div>
             <div class="row">
                 <div class="col-md-offset-2"></div>
@@ -50,15 +58,11 @@ function all_random_names ($arr1, $arr2)
                     <button class='btn btn-primary' id='server-name'>Server Name</button>
                 </div>
                 <div class="col-md-4">
-                    <p class='lead' id='random-name'><?php var_dump(all_random_names($adjs, $nouns)); ?></p>
+                    <p class='' id='random-name'><?php var_dump(pageController()); ?></p>
                 </div>
                 <div class="col-md-offset-3"></div>
             </div>
         </div>
-        <script>
-            $('server-name').on('click', function() {
-                // $('#random-name').html();
-            })
-        </script>
+        <script></script>
     </body>
 </html>
