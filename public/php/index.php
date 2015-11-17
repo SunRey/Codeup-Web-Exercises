378e6450b9cc2619b324e5be9ee8b4c50c1a7827
Jonathan R <jreyes.satx@gmail.com>
2015-11-17 15:26:21 -0600

    website templates for php. will work on more for all the templates.

---
+++ public/php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <link href="/css/sticky-footer.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+            <?php include 'navbar.php' ?>
+        <div class="container">
+            <?php include 'body.php' ?>
+        </div>
+            <?php include 'footer.php' ?>
+        <script></script>
+    </body>
+</html>
