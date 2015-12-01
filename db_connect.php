9a7f31d888670b01d952342949bbeec3eef16043
Jonathan R <jreyes.satx@gmail.com>
2015-12-01 15:48:11 -0600

    db connect file. cleanned up

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,6 @@
 <?php
-require 'db_constant_test.php';
 
-$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . '', '' . DB_USER .'', '' . DB_PASSWORD .'');
+$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
 
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
