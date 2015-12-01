3ecf627678fc8c7867541ad94da71f7be0bd1516
Jonathan R <jreyes.satx@gmail.com>
2015-12-01 14:27:29 -0600

    begin of parks exercise. created db with php migration. NEXT: INSERT into db

---
+++ park_migration.php
@@ -0,0 +1,21 @@
+<?php 
+require 'parks_db_constants.php';
+require 'db_connect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$dbc->exec('DROP TABLE IF EXISTS national_parks;');
+
+$createTable = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(75) NOT NULL,
+    location VARCHAR(100) NOT NULL, 
+    date_established YEAR(4) NOT NULL,
+    area_in_acres DOUBLE(10, 2),
+    PRIMARY KEY (id)
+);';
+
+$dbc->exec($createTable);
+
+
+?>
